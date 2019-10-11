cb5c75f67077684dc3ac6e5849886e1071aeeb8a
Richard CORENTIN <rcorenti@e1r12p7.42.fr>
2019-10-11 20:04:20 +0200

    start part 2

--- part2/ft_strjoin.c
+++ part2/ft_strjoin.c
@@ -1,38 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rcorenti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/11 16:06:10 by rcorenti          #+#    #+#             */
-/*   Updated: 2019/10/11 19:49:15 by rcorenti         ###   ########.fr       */
+/*   Created: 2019/10/11 19:49:33 by rcorenti          #+#    #+#             */
+/*   Updated: 2019/10/11 20:03:51 by rcorenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-char	*ft_substr(const char *s1, unsigned int start, size_t len)
+char	*ft_strjoin(const char *s1, const char *s2)
 {
-	size_t		size;
-	size_t		i;
-	char		*str;
-	unsigned int	k;
+	char	*str;
+	size_t	i;
+	size_t	size;
 
-	size = ft_strlen(s1) - (size_t)start;
-	i = 0;
 	str = NULL;
-	k = start;
-	if (start <= 0 && len <= 0)
-		return (NULL);
-	if (size > len)
-		size = len;
+	i = 0;
+	size = ft_strlen(s1) + ft_strlen(s2);
 	if (!(str = malloc(sizeof(char) * size)))
 		return (NULL);
 	while (i < size)
 	{
-		str[i] = s1[k];
-		k++;
+		
 		i++;
 	}
 	return (str);
