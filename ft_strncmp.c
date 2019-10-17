4763e3032b81987054a2a7d5fe33a31e1925f128
System Administrator <root@corentins-iMac.local>
2019-10-16 18:23:10 -0700

    mods fts

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,27 +1,28 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: rcorenti <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/16 00:49:42 by rcorenti          #+#    #+#             */
-/*   Updated: 2019/10/16 08:21:18 by rcorenti         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1,const char *s2,size_t n)
 {
-	size_t	i;
+	int		i;
+	int		x;
+	size_t	k;
 
-	i = 0;
-	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i < n - 1)
-		i++;
-	if (n == 0)
+	x = 0;
+	k = n;
+	if (k == 0)
 		return (0);
-	if (s1[i] == '\0' || s2[i] == '\0')
-		i--;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	while(k != 0) 
+	{
+		if (s1[x] != s2[x])
+		{
+			i = (int)((unsigned char)s1[x] - (unsigned char)s2[x]);
+			if (i < 0)
+				return (-1);
+			if (i > 0)
+				return (1);
+			return (0);
+		}
+		k--;
+		x++;
+	}
+	return (0);
 }
--- ft_substr.c
+++ ft_substr.c
@@ -2,32 +2,31 @@
 
 char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
-	char *newchain;
-	size_t x;
-	int temp;
-	size_t ssize;
+char *newchain;
+size_t x;
+int temp;
+size_t ssize;
 
-	x = 0;
-	temp = 0;
-	ssize = 0;
-	while (s[ssize])
-		ssize++;
-	if(len == 0 || start > ssize || s[ssize])
-		return (NULL);
+x = 0;
+temp = 0;
+ssize = 0;
+while (s[ssize])
+ssize++;
+if(start > ssize || s[ssize])
+return (NULL);
 
-	if (len <= ssize - start)
-		temp = (int)len + 1;
-	if (len > ssize - start)
-		temp = (int)ssize - start;
-	if (!(newchain = malloc(sizeof(char) * temp)))
-		return (NULL);
-	while(len > 0 && s[x + start])
-	{
-		newchain[x] = s[x + start - 1];
-		x++;
-		len--;
-	}
-	newchain[x + start] = '\0';
-	return (newchain);
+if (len <= ssize - start)
+temp = (int)len + 1;
+if (len > ssize - start)
+temp = (int)ssize - start;
+if (!(newchain = malloc(sizeof(char) * temp)))
+return (NULL);
+while(len > 0 && s[x + start])
+{
+newchain[x] = s[x + start];
+x++;
+len--;
 }
-
+newchain[x + start] = '\0';
+return (newchain);
+}
