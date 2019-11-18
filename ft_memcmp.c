278b1d12909669316ec06c4849ad6d715c175b7a
corentin richard <rcorenti@corentins-iMac.local>
2019-11-18 10:24:05 -0800

    final

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -18,14 +18,14 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	unsigned char	*str1;
 	unsigned char	*str2;
 
-	if (n == 0)
+	if (n <= 0)
 		return (0);
 	i = 0;
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
 	while (i < n)
 	{
-		if (str1[i] != str2[i] || str1[i] == '\0')
+		if (str1[i] != str2[i])
 			return ((int)(str1[i] - str2[i]));
 		i++;
 	}
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -19,7 +19,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	x = 0;
 	if (n <= 0)
 		return (0);
-	while (x < (n - 1) && s1[x] == s2[x])
+	while (x < (n - 1) && s1[x] == s2[x] && s1[x] && s2[x])
 		x++;
 	return ((unsigned char)s1[x] - (unsigned char)s2[x]);
 }
